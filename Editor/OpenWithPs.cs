009bc2bf54a92551ccb8ccf0925df6ce5c8d4e3d
david-sigaty <david@sigaty.dev>
2020-01-29 09:48:34 -0700

    Initial commit.

---
+++ Editor/OpenWithPs.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using TeamCitrus.Editor;
+
+public class OpenWithPs : OpenWithMenuItem
+{
+	public const string Name = "Photoshop";
+	public const string Key = BaseKey + Name;
+	private const string MenuItemPath = BasePath + Name;
+	
+	[MenuItem(MenuItemPath, false, BasePriority + 50)]
+	public static void Open() { Execute<OpenWithPs>(); }
+
+	[MenuItem(MenuItemPath, true)]
+	public static bool OpenValidation() { return Validate<OpenWithPs>(); }
+}
